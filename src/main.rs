f0243447515b88fda5e95abbf24079df55d0ba77
laixi.xu <laixi.xu@timeresearch.com>
2023-03-08 11:46:59 +0800

    update every 1s

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,18 @@ mod prices;
 
 use wasm_bindgen_futures::spawn_local;
 
+async fn sleep(delay: i32) {
+    let mut cb = |resolve: js_sys::Function, _: js_sys::Function| {
+        let _ = web_sys::window()
+            .unwrap()
+            .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, delay);
+    };
+
+    let p = js_sys::Promise::new(&mut cb);
+
+    wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
+}
+
 fn main() -> std::io::Result<()> {
     spawn_local(async {
         let window = web_sys::window().expect("no global `window` exists");
@@ -10,17 +22,21 @@ fn main() -> std::io::Result<()> {
         let body = document.body().expect("document should have a body");
 
         let node = document.create_element("div").unwrap();
-
-        let plot = plot::plot().await;
-
-        node.set_inner_html(&plot.to_inline_html(None));
         body.append_child(node.as_ref()).unwrap();
-        let script = node
-            .get_elements_by_tag_name("script")
-            .item(0)
-            .unwrap()
-            .inner_html();
-        js_sys::eval(&script).unwrap();
+
+        loop {
+            let plot = plot::plot().await;
+            node.set_inner_html(&plot.to_inline_html(None));
+
+            let script = node
+                .get_elements_by_tag_name("script")
+                .item(0)
+                .unwrap()
+                .inner_html();
+            js_sys::eval(&script).unwrap();
+
+            sleep(1000).await;
+        }
     });
 
     Ok(())
