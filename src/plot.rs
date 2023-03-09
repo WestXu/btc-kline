8fe472cd417f726484d22ca17bf8a8f3ffda34e5
laixi.xu <laixi.xu@timeresearch.com>
2023-03-09 13:07:38 +0800

    plot: traces naming

--- src/plot.rs
+++ src/plot.rs
@@ -34,12 +34,13 @@ pub async fn plot(kline_data: &Vec<Kline>, dark: bool) -> Plot {
 
     let fit_trace = best_fit(&close);
 
-    let trace = Candlestick::new(x.clone(), open, high, low, close);
+    let trace = Candlestick::new(x.clone(), open, high, low, close).name("Kline");
     let fit_trace = plotly::Scatter::new(x, fit_trace)
-        .line(plotly::common::Line::new().dash(plotly::common::DashType::Dash));
+        .line(plotly::common::Line::new().dash(plotly::common::DashType::Dash))
+        .name("PolyFit");
 
     let mut plot = Plot::new();
-    plot.add_trace(trace);
+    plot.add_trace(Box::new(trace));
     plot.add_trace(fit_trace);
     let layout = Layout::new()
         .title(
